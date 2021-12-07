ec88e526a465f17ba5eb484d41b3d80558c90a5c
gary <gary.white@lodgit.com>
2021-12-07 16:46:12 +0100

    minor change

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// document.querySelector("h1"); // standard JS
+// $("h1") // Same query using jQuery
+
 $(document).ready(function() { // This function waits for jquery to be loaded before proceeding
     $("h1").css("color", "red"); // $ = jQuery
 });

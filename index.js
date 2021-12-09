f8cded48a63325fe97b260b99461d5069fec3176
gary <gary.white@lodgit.com>
2021-12-09 15:49:01 +0100

    changes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-// document.querySelector("h1"); // standard JS
-// $("h1") // Same query using jQuery
+/*document.querySelector("h1"); // standard JS
+$("h1") // Same query using jQuery
 
-$(document).ready(function() { // This function waits for jquery to be loaded before proceeding
-    $("h1").css("color", "red"); // $ = jQuery
-});
+document.querySelectorAll("button") //JS
+$("button") // JQ
+
+$("h1").css("color", "green");
+*/
+
+// $(document).ready(function() { // This function waits for jquery to be loaded before proceeding
+//     $("h1").css("color", "red"); // $ = jQuery
+// });
+
+// $("h1").css("color", "green"); // Font to green
+// $("h1").css("font-size", 115); // change font size
+
+$("h1").addClass("big-title")
+// $("h1").removeClass("big-title")

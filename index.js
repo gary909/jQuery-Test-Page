209f7589e9f6a2b17e3dc1f3b10322226eb01f9f
gary <gary.white@lodgit.com>
2021-12-09 15:57:16 +0100

    changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Don't forget to import the jQuery library in the html
+
 /*document.querySelector("h1"); // standard JS
 $("h1") // Same query using jQuery
 
@@ -16,3 +18,12 @@ $("h1").css("color", "green");
 
 $("h1").addClass("big-title")
 // $("h1").removeClass("big-title")
+
+$("h1").text("Bye") // changes the text from 'hello' to 'bye'
+
+//$("button") // selects all the elements that match the selector
+
+$("button").text("Don't Click Me") // Changes all the buttons text to 'Don't click Me'
+
+$("button").html("<em>Hey</em>") // changes the buttons html
+
